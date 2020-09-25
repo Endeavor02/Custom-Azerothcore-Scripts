90fc38cf9f6a2ae8f59a2d01b056a0a8e8bd4578
Jeffrey Blanda <41349027+Endeavor02@users.noreply.github.com>
2020-09-25 11:25:51 -0400

    Update blackwing_lair.h

--- blackwing_lair.h
+++ blackwing_lair.h
@@ -3,7 +3,7 @@
  * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
  * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
  */
-
+// Modified 2020 Jeffrey Blanda
 #ifndef DEF_BLACKWING_LAIR_H
 #define DEF_BLACKWING_LAIR_H
 
